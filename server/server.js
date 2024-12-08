8756e38007ff0f73ba977a4e82bccec8e344763f
Kyle8Bits <khoamaidang2611@gmail.com>
2024-12-09 00:15:45 +0700

    test

--- server/server.js
+++ server/server.js
@@ -9,17 +9,8 @@ app.use(express.json());
 
 connectDB()
 
-app.get('/', async (req, res) => {
-  try {
-    const signals = await Signal.find(); // Fetch all signals from the database
-    res.json(signals);  // Send the data as a response
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Error fetching signals' });
-  }
+app.use('/', (req, res) => {
+  res.send("Sever is running");
 });
 
-const PORT = 1414;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(1414, console.log("Server start port 1414"))
