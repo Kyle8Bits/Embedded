28323d6c32906105798439431c312f05eb589588
Kyle8Bits <khoamaidang2611@gmail.com>
2024-12-09 00:16:58 +0700

    test 2

--- server/server.js
+++ server/server.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const app = express();
-const Signal = require('./models/Signal');
-const connectDB = require('./config/db');
-app.use(cors());
-app.use(express.json());
 
-connectDB()
+
+const app = express()
+
 
 app.use('/', (req, res) => {
   res.send("Sever is running");
