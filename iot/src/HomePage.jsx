46ad696e52ad6f0d45a47a52e878a5dd22548167
Kyle8Bit <khoamaidang2611@gmail.com>
2024-12-16 00:55:52 +0700

    fix 2

--- iot/src/HomePage.jsx
+++ iot/src/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './style.css'
 import Iot from './images/iot.png'
+import banner from './images/banner.png'
 import Avatar from './images/avatar.png'
 import { useNavigate } from 'react-router-dom'
 
@@ -15,16 +16,18 @@ function HomePage() {
         <div id="welcomelabel">Welcome to IOT</div>
 
         <div id="dashboard_image">
-            <img src={Iot} alt="" id="my_img" />
-            <p id="des">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tempus eros. Nulla tincidunt feugiat blandit. Nullam rhoncus turpis et augue consectetur tincidunt. In hac habitasse platea dictumst. Aenean mollis laoreet tempor. Nullam porta, dolor at sagittis dapibus, leo mi sagittis ipsum, ut bibendum diam metus nec justo. In nec suscipit nulla. Maecenas quis sem volutpat tortor gravida auctor vitae id leo. Duis vitae euismod libero. Vestibulum aliquet vehicula suscipit. Aliquam neque neque, imperdiet quis fringilla sit amet, placerat eu libero. Nulla facilisi.</p>
+            <img src={banner} alt="" id="my_img" />
+            <h1 id="h1">We All About</h1>
+            <h1 id="h2">TAKE FULL CONTROL</h1>
+            <h1 id="h3">of Your Home</h1>
+            <button id="navigate" onClick={handleNavigate}> Go To Controller</button>
         </div>
 
         <div id="student_info">
             <div id="information">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tempus eros orem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tempus eros. orem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tempus eros. orem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tempus eros. orem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tempus eros. orem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tempus eros.  </div>
             <img src={Avatar} alt="" id="avatar" />
-            <button id="navigate" onClick={handleNavigate}> Go To Controller</button>
-        </div>
 
+        </div>
     </div>
   )
 }
